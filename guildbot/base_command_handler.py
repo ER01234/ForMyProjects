902f8df20ee8e237f7ceaa451b1e88d782c730d8
Roman Evseenko <53880474+ER01234@users.noreply.github.com>
2026-05-03 09:32:35 +0300

    Delete guildbot directory

--- guildbot/base_command_handler.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Optional, Any
-from vkbottle.bot import Message
-
-class BaseCommandHandler(ABC):
-    @abstractmethod
-    async def handle(self, message: Message) -> Optional[str]:
-        pass
--- guildbot/dev_aiohttp_client.py
+++
@@ -1,6 +0,0 @@
-from vkbottle.http import SingleAiohttpClient
-
-class DevAiohttpClient(SingleAiohttpClient):
-    async def request_raw(self, url, method: str = "GET", data=None, **kwargs):
-        kwargs.setdefault("ssl", False)
-        return await super().request_raw(url, method, data, **kwargs)
